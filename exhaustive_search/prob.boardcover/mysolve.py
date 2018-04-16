739f6d0c3fa7032113b89d1893e3e73097862c00
cheese <cheese.choi@gmail.com>
2018-04-16 11:17:13 +0900

    [+] add my solution - boardcover

--- exhaustive_search/prob.boardcover/mysolve.py
+++ exhaustive_search/prob.boardcover/mysolve.py
@@ -1,4 +1,5 @@
 import copy
+import time
 
 case = 0
 arrHeight = []
@@ -28,62 +29,56 @@ for i in range(0, case):
 
 f.close()
 
+
 BLOCKS = [
-			[(-1, -1), (0, -1), (0,0)],
-			[(0, -1), (0,0), (-1, 0)],
-			[(-1, -1), (-1,0), (0, 0)],
-			[(-1, 0), (-1,-1), (0, -1)]
+			[(0, 0), (0, 1), (-1,1)],
+			[(0, 0), (1,0), (1, 1)],
+			[(0, 0), (0,1), (1, 1)],
+			[(0, 0), (1,0), (0, 1)]
 		]
 
 
-def solver(maxx, maxy, x, y, mapdata):
+def solver(maxx, maxy, mapdata):
 
+	#maxy = 6
 
+	nBlank = ''.join(mapdata).count('.')
 
+	if nBlank%3 != 0: return 0
+	if nBlank == 0: return 1
+
+	ptX = (''.join(mapdata).find('.')%len(mapdata[0]))
+	ptY = (''.join(mapdata).find('.')/len(mapdata[0]))
+
+	#print mapdata[ptY][ptX]
+	#return 1
 	ret = 0
-
-	if "." not in ''.join(mapdata):
-		return 1
-	if y > maxy:
-		return 0
 
 	print '==='
 	for low in mapdata: print low
-	print "x %d, y %d"%(x, y)
-	print mapdata[y][x]
-	#print mapdata[y]
+	print "x %d, y %d"%(ptX, ptY)
 
-	isFillFailed = True
+
+
 	for block in BLOCKS:
 		mapNext = copy.deepcopy(mapdata)
-		canSet = True
 
+		canSet = True
 		for pair in block:
 			#print pair
 			#print "%d %d"%(x+pair[0],y+pair[1])
-			if mapNext[y+pair[1]][x+pair[0]] == "#":
+			if mapNext[ptY+pair[1]][ptX+pair[0]] == "#":
 				canSet = False
 				break
 
-			else:
-				#mapNext[y+pair[1]][x+pair[0]] = "#"
-				low = mapNext[y+pair[1]]
-				#print '---'
-				#print "%s, %d"%(low, x+pair[0])
-				low = low[:x+pair[0]]+"#"+low[x+pair[0]+1:]
-				#print low
-				mapNext[y+pair[1]] = low
+			low = mapNext[ptY+pair[1]]
+			low = low[:ptX+pair[0]]+"#"+low[ptX+pair[0]+1:]
+			mapNext[ptY+pair[1]] = low
 
 		if canSet:
-			print 'lets paint '+str(block)
+			isFillFailedAll = False
+			ret += solver(maxx, maxy, mapNext )
 
-			isFillFailed = False
-			ret += solver(maxx, maxy, x+1 if (x+1)<maxx else 1, y+1 if (x+1)>maxx else y, mapNext )
-	
-	if isFillFailed:
-		print 'failed. next step!'
-
-		ret += solver(maxx, maxy, x+2 if (x+2)<maxx else 1, y+1 if (x+2)>maxx else y, mapdata )
 
 	return ret
 
@@ -93,14 +88,20 @@ def boardcover():
 	for nStage in range(0, case):
 		print 'solve %d'%nStage
 		print 'width : %d, height : %d'%(arrWidth[nStage], arrHeight[nStage])
-		print 'fairs : '+str(arrMaps[nStage])
+		print '- fairs '
+		for i in arrMaps[nStage]: print i
 
-
-		print solver(arrWidth[nStage]-1, arrHeight[nStage]-1, 1, 1, arrMaps[nStage])
+		print 'result : %d'%solver(arrWidth[nStage]-1, arrHeight[nStage]-1, arrMaps[nStage])
 
 	return
 
 
 if __name__ == "__main__":
+	startTime = time.time()
+
 	print "hi solver"
 	boardcover()
+
+	endTime = time.time() - startTime
+	print(endTime) 
+
