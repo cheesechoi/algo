8f09041448ccac9255b0d3b26fb2a87ed888866d
cheese <cheese.choi@gmail.com>
2018-10-11 16:55:13 +0900

    [+] code new way - wildcard

--- dynamic_programming/wildcard.py
+++ dynamic_programming/wildcard.py
@@ -12,7 +12,7 @@ ex ) 	he?p --> help(o) heap(o) helpp(x)
 파일명 집합이 주어질 때, 패턴에 대응되는 파일명을 찾애는 프로그램을 작성하라.
 '''
 
-from pwn import *
+#from pwn import *
 
 example = '''
 	3
@@ -30,5 +30,62 @@ example = '''
 	1
 	babbbc
 	'''
+
+def nop(): return
+
+def Wildcard(pattern, sample):
+	print 'pattern : %s, sample : %s'%(pattern, sample)
+
+	pidx = sidx = 0
+	while ( (pidx < len(pattern)) and (sidx < len(sample)) ) \
+			and ((pattern[pidx] == '?') or (pattern[pidx] == sample[sidx])):
+
+		pidx += 1
+		sidx += 1
+
+
+
+
+	if (pidx >= len(pattern)) or (sidx >= len(sample)):
+		return (pidx >= len(pattern)) and (sidx >= len(sample))
+
+	print '%s vs %s'%(pattern[pidx:], sample[sidx:])
+
+
+	return True
+
+def solver():
+	global example
+	example = [i.strip() for i in example.strip().split('\n')]
+	example = example[::-1]
+
+	case = int(example.pop())
+	print 'case : %d'%case
+	
+	for nStage in range(1, case+1):
+		print '---'
+		result = 0
+		_pattern = example.pop()
+		_nValue = int(example.pop())
+
+		for _ in range(0, _nValue):
+			_sample = example.pop()
+
+			if Wildcard(_pattern, _sample):
+				result += 1
+
+		#print 'solve %d'%nStage
+		#print 'frnds : %d, fairs : %d'%(nFriends[nStage], nFairs[nStage])
+		#print 'fairs : '+str(arrFairs[nStage])
+
+		
+		print 'result : %d'%result
+	return
+
+
+if __name__ == "__main__":
+	print "hi solver"
+	solver()
+
 
 
